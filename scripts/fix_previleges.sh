67909b2fc93e8c4d24e76acdbe64ffb54138cac5
VishalTalatii <x20131020@student.ncirl.ie>
2020-11-21 13:24:42 +0530

    chnages

--- scripts/fix_previleges.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-chmod +x /home/ec2-user/springboot-crud-api-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/start_server.sh
-chmod +x /home/ec2-user/stop_server.sh
-
--- scripts/stop_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# pkill -f 'java -jar'
-
-#!/bin/bash
-java -jar -Dspring.profiles.active=dev /home/ec2-user/jspringboot-crud-api-0.0.1-SNAPSHOT.jar &
